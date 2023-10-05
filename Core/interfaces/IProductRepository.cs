7c47876151afa62eff035d21ee3e4c68c8528bfa
User <User@F4632L2384U2475>
2023-10-06 02:15:29 +0300

    End of section 3

---
+++ Core/interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Core.interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetTypesAsync();
+
+    }
+}

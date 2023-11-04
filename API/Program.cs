c977d9ccae9b3b7a5e328b4301fa8af47725884d
Sinkov Andrey <sinkov407@gmail.com>
2023-11-04 20:13:37 +0300

    Start client development

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-
 app.UseMiddleware<ExeptionMiddleWare>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");

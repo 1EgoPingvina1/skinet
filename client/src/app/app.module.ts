66675f01d47078645ce03ecf63f5160657f6178a
Sinkov Andrey <sinkov407@gmail.com>
2023-11-06 20:10:20 +0300

    Add product details and lazy loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule} from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
-import { ShopComponent } from './shop/shop.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,7 @@ import { ShopComponent } from './shop/shop.component';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
